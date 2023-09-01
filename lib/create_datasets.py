ecc4b00ca91b4b0274c64875f4cdd9fe23b529fb
Matteo Romanello <matteo.romanello@epfl.ch>
2023-09-01 11:17:32 +0300

    regenerated corpus (biblio layer), version 0.4

--- lib/create_datasets.py
+++ lib/create_datasets.py
@@ -122,27 +122,6 @@ def create_datasets(input_dir, output_dir, version, assignments_table_path, set=
                 dataset_path = create_dataset(
                     document_paths, lang, split, version, output_dir
                 )
-
-                if split == "test":
-                    # generate a version of the test dataset with all ground truth values masked out
-                    tsv_data = parse_tsv(file_path=dataset_path, mask_nerc=True, mask_nel=True)
-                    masked_dataset_name = os.path.basename(dataset_path).replace(
-                        "-test", "-test_allmasked"
-                    )
-                    masked_dataset_path = os.path.join(
-                        output_dir, version, masked_dataset_name
-                    )
-                    write_tsv(tsv_data, masked_dataset_path)
-                    
-                    # generate a version of the test dataset with EL ground truth values masked out
-                    tsv_data = parse_tsv(file_path=dataset_path, mask_nel=True, mask_nerc=False)
-                    masked_dataset_name = os.path.basename(dataset_path).replace(
-                        "-test", "-test-ELmasked"
-                    )
-                    masked_dataset_path = os.path.join(
-                        output_dir, version, masked_dataset_name
-                    )
-                    write_tsv(tsv_data, masked_dataset_path)
                     
 
             # for each language, read the list of noisy entities
